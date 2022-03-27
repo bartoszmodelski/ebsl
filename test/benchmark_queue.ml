baf30cad10b5b70543882bf2e46de4fdfcc79bb4
model <modelski.bartosz@gmail.com>
2022-03-27 13:56:53 +0100

    q bench cmd

---
+++ test/benchmark_queue.ml
@@ -0,0 +1,69 @@
+
+let total_rounds = 8
+let bench ~num_of_domains ~item_count (module Queue : Datastructures.Queue_intf.S) = 
+  assert (num_of_domains mod 2 == 0);
+  Random.init 0;
+  let spawned = Atomic.make 0 in 
+  let _done = Atomic.make 0 in 
+  let current_round = Atomic.make (-1) in 
+  let per_thread_item_count = item_count / num_of_domains in 
+  let queue = Queue.init () in
+  let domains = 
+    List.init num_of_domains (fun i -> 
+      Domain.spawn (fun () -> 
+        Atomic.incr spawned; 
+        let round = ref 0 in 
+        while !round < total_rounds do 
+          while (Atomic.get current_round) < !round do () done;
+          for _ = 0 to per_thread_item_count do 
+            if i mod 2 == 0 then 
+              Queue.enqueue queue 0
+            else
+              while Option.is_none (Queue.dequeue queue) do () done;
+          done;
+          Atomic.incr _done;
+          round := !round + 1;
+        done))
+  in
+  while Atomic.get spawned < num_of_domains do () done;
+  let times = ref [] in 
+  while (Atomic.get current_round)+1 < total_rounds do 
+    let before = Schedulr.Fast_clock.now () in 
+    Atomic.incr current_round; 
+    while Atomic.get _done < num_of_domains do () done; 
+    let after = Schedulr.Fast_clock.now () in
+    Atomic.set _done 0;
+    let diff =  Base.Int63.(to_int64(after-before)) in 
+    times := diff :: !times
+  done;
+  Atomic.incr current_round;
+  List.iter Domain.join domains;
+  !times;;
+
+
+let () =
+  let usage_msg = "benchmark -queue (LF|LOCK) -num-of-domains <num>" in 
+  let anon_fun _ = failwith "no anon parameters expected" in
+  let queue = ref "" in
+  let num_of_domains = ref 0 in 
+  let speclist =
+    [("-queue", Arg.Set_string queue, "set scheduler algo");
+    ("-num-of-domains", Arg.Set_int num_of_domains, "set num of domains")] 
+  in 
+  Arg.parse speclist anon_fun usage_msg;
+  let queue_module =
+      match !queue with 
+      | "LF" -> (module Datastructures.Mpmc_queue : Datastructures.Queue_intf.S)
+      | "LOCK" -> (module Datastructures.Lock_queue)
+      | _ -> assert false
+  in 
+  assert (0 < !num_of_domains && !num_of_domains < 512);
+  let num_of_domains = !num_of_domains in
+  let times = 
+    bench ~num_of_domains ~item_count:1_000_000 queue_module
+  in
+  let times = 
+    List.map (fun v -> Int64.div v 1_000_000L) times 
+  in
+  List.iter (fun v -> 
+    Printf.printf "time: %Ld ms\n" v) times;;
