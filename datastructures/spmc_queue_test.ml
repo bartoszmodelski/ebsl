11607f05bcc6cdf7c765cf9a44481bb2c7e2ab2b
model <modelski.bartosz@gmail.com>
2022-03-31 14:57:53 +0100

    expect tests

--- datastructures/spmc_queue_test.ml
+++ datastructures/spmc_queue_test.ml
@@ -40,3 +40,73 @@ let%expect_test _ =
      data: _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _
 
     dequeued: 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 |}];;
+
+
+let%expect_test _ = 
+  let q = (Spmc_queue.init ~size_exponent:4 () : int Spmc_queue.t) in 
+  let q_2 = (Spmc_queue.init ~size_exponent:4 () : int Spmc_queue.t) in 
+  for i = 0 to 14 do  
+    assert (Spmc_queue.local_enqueue q i); 
+  done;
+  dump_spmc q;
+  [%expect {|
+    head: 0, tail 15
+     data: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, _ |}];
+  for i = 1 to 5 do 
+    let stolen = Spmc_queue.steal ~from:q ~to_local:q_2 in 
+    Printf.printf "=====\nSteal #%d. Stole %d items.\n" i stolen;
+    dump_spmc q; 
+    Printf.printf "--->\n";
+    dump_spmc q_2;
+    Printf.printf "\n";
+  done;
+  [%expect {|
+    =====
+    Steal #1. Stole 8 items.
+    head: 8, tail 15
+     data: _, _, _, _, _, _, _, _, 8, 9, 10, 11, 12, 13, 14, _
+
+    --->
+    head: 0, tail 8
+     data: 0, 1, 2, 3, 4, 5, 6, 7, _, _, _, _, _, _, _, _
+
+
+    =====
+    Steal #2. Stole 4 items.
+    head: 12, tail 15
+     data: _, _, _, _, _, _, _, _, _, _, _, _, 12, 13, 14, _
+
+    --->
+    head: 0, tail 12
+     data: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, _, _, _, _
+
+
+    =====
+    Steal #3. Stole 2 items.
+    head: 14, tail 15
+     data: _, _, _, _, _, _, _, _, _, _, _, _, _, _, 14, _
+
+    --->
+    head: 0, tail 14
+     data: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, _, _
+
+
+    =====
+    Steal #4. Stole 1 items.
+    head: 15, tail 15
+     data: _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _
+
+    --->
+    head: 0, tail 15
+     data: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, _
+
+
+    =====
+    Steal #5. Stole 0 items.
+    head: 15, tail 15
+     data: _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _
+
+    --->
+    head: 0, tail 15
+     data: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, _ |}];;
+    
