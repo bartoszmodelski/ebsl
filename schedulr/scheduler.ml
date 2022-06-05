42510d9736f22a4f91cf8f1951e9bd75581a4795
model <modelski.bartosz@gmail.com>
2022-06-05 20:14:45 +0100

    options 2

--- schedulr/scheduler.ml
+++ schedulr/scheduler.ml
@@ -105,7 +105,7 @@ module Make (DS : DataStructure) = struct
       waited_for_space_on_enque := 0;;
 
     let take_from {steal_attempts; suggest_steal; _} global_requesting_queue = 
-      let force_use_queue = Random.int 100 < 100 in 
+      let force_use_queue = Random.int 100 < 50 in 
       let steal_or_f v = 
         steal_attempts := !steal_attempts + 1;
         if !steal_attempts mod 4 == 0 then 
